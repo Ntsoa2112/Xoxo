a622f6b1ff270e2b92ee6c0da9743fdc4e90d96c
ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2022-07-06 17:04:18 +0300

    Add rectangle

--- app/src/main/java/com/example/xoxo/view/MatriceView.java
+++ app/src/main/java/com/example/xoxo/view/MatriceView.java
@@ -10,6 +10,9 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class MatriceView extends View {
+
+    private int dim = 4;
+
     public MatriceView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
@@ -21,10 +24,27 @@ public class MatriceView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint newPaint = new Paint();
-        newPaint.setColor(Color.BLACK);
-        canvas.drawRect(100, 100,  60, 60, newPaint);
+        Paint carre = new Paint();
+        carre.setStyle(Paint.Style.STROKE);
+        carre.setColor(Color.BLACK);
+        int height = getLayoutParams().height;
+        int width = getLayoutParams().width;
+        int abscisse = (int)width/(dim+2);
+        int ordonnee = (int)height/(dim+2);
+        for(int x = 0; x<this.dim*230; x+=230){
+            for(int y = 0; y<this.dim*230; y+= 230){
+                canvas.drawRect(abscisse+ x, abscisse + y,  ordonnee + x, ordonnee + y, carre);
+            }
+        }
 
+
+
+        // stroke
+        /*
+        strokePaint.setStyle(Paint.Style.STROKE);
+        strokePaint.setColor(Color.BLACK);
+        strokePaint.setStrokeWidth(10);
+        */
     }
 
 
